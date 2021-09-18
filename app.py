c33b136a757cc2981eecf41cedb0720a7457601f
vishwajeet <vishwajeetjagtap1999@gmail.com>
2021-09-19 01:35:36 +0530

    Face Detection

--- app.py
+++ app.py
@@ -98,20 +98,27 @@ elif add_selectbox=="Face mesh":
             mp_drawing.draw_landmarks(image,face_landmarks)
         st.image(image)
 elif add_selectbox=="Face Detection":
-    st.write("Detecting face")
 
     class VideoTransformer(VideoTransformerBase):
+        def __init__(self):
+            self.threshold1 = 100
+            self.threshold2 = 200
+
         def transform(self, frame):
             img = frame.to_ndarray(format="bgr24")
-            results=model_detection.process(img)
-            for landmarks in results.detections:
-                mp_drawing.draw_detection(img,landmarks)
 
+            img = cv2.cvtColor(
+            cv2.Canny(img, self.threshold1, self.threshold2), cv2.COLOR_GRAY2BGR
+            )
 
             return img
 
 
-    webrtc_streamer(key="example", video_transformer_factory=VideoTransformer)
+    ctx = webrtc_streamer(key="example", video_transformer_factory=VideoTransformer)
+
+    if ctx.video_transformer:
+        ctx.video_transformer.threshold1 = st.slider("Threshold1", 0, 1000, 100)
+        ctx.video_transformer.threshold2 = st.slider("Threshold2", 0, 1000, 200)
     
         
     
